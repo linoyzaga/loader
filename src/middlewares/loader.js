ebb719f9c3c8676f1672fb23b416fd7db037e13d
Linoy Zaga <linoyzaga@gmail.com>
2019-11-25 21:49:48 +0200

    task 2 - Batching

--- src/middlewares/loader.js
+++ src/middlewares/loader.js
@@ -1,6 +1,28 @@
-const createLoader = (getMany) => {
+function getOne(ids, getMany) {
+    return getMany(ids);
+};
+
+const createLoader = (getMany, options = {batchInterval: 0}) => {
+    const interval = options.batchInterval;
+    const resolves = [];
+    
+    var ids = [];
+    var result;
+
+    setInterval(async() => {
+        if (ids.length !== 0) {
+            result = await getOne(ids, getMany);
+            ids = [];
+            resolves.map(res => res(result));
+        }
+    }, interval);
+
     return function(id) {
-        return getMany([id]);
+        ids.push(id);
+
+        return new Promise((res) => {
+            resolves.push(res);
+        });;
     };
 };
 
--- src/server.js
+++ src/server.js
@@ -9,9 +9,31 @@ app.listen(PORT, function() {
     console.log(`Server running on ${PORT}`);
 });
 
-const getOneUser = createLoader(getManyUsers);
+// First Task
+const getOneUser1 = createLoader(getManyUsers);
 (async function main(){
-    console.log(await getOneUser(1));
-    console.log(await getOneUser(4));
+        console.log(await getOneUser1(1));
+        console.log(await getOneUser1(4));
     }
 )();
+
+// Second Task
+const getOneUser2 = createLoader(getManyUsers, { batchInterval: 100 });
+
+(async function() {
+    const user = getOneUser2(1);
+
+    await new Promise((resolve) => setTimeout(resolve, 50))
+
+    const rest = await Promise.all([getOneUser2(2), getOneUser2(3), getOneUser2(4)]);
+})();
+
+const getOneUser3 = createLoader(getManyUsers, { batchInterval: 100 });
+
+(async function() {
+    const user = getOneUser3(1);
+
+    await new Promise((resolve) => setTimeout(resolve, 150))
+
+    const rest = await Promise.all([ getOneUser3(2), getOneUser3(3), getOneUser3(4)]);
+})();
